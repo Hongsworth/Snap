d7a8be8a2247cb940d7215d8f4b17f504a78c3e5
Hongsworth <therchgaming@gmail.com>
2026-02-10 14:39:53 +0000

    added the ability to contine the game after ending

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,23 @@
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
+    static boolean cont = true;
+    static Scanner scanner = new Scanner(System.in);
+    static Snap snap;
     public static void main() throws IOException {
-        Snap snap = new Snap();
-        snap.runGame();
+        while (cont){
+            snap = new Snap();
+            snap.runGame();
+            System.out.println("Play again?");
+            System.out.println("Enter y to continue and anything else to exit");
+            if(!Objects.equals(scanner.nextLine(), "y")){
+                cont = false;
+            }
+            else{
+                System.out.println("Game ending");
+            }
+        }
     }
 }
